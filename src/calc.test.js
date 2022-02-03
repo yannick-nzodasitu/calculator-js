864e13a079f6f5a6d4a039a27405038bc7c4231d
Yannick Nzoda Situ <yannick.nzodasitu@cgi.com>
2022-02-03 08:10:19 -0500

    delete describe

--- src/calc.test.js
+++ src/calc.test.js
@@ -16,17 +16,17 @@ afterAll(()=> {
 })   
 
 test('Adding 2 + 3 should give me 5', () => {
-    expect(sum(2, 3)).toBe(5);
+    expect(sum(4, 3)).toBe(7);
 })
 
 test('Subtract 2 from 3 that should give me a 1', () => {
     expect(subtract(3, 2)).toBe(1);
 })
 
-describe('sum',()=>{
-    each([
+// describe('sum',()=>{
+//     each([
 
-    ]).test('sum %s and %s should be %s', (a,b,result) => {
-        expect(sum(a,b)).toBe()
-    })
-})
+//     ]).test('sum %s and %s should be %s', (a,b,result) => {
+//         expect(sum(a,b)).toBe()
+//     })
+// })
