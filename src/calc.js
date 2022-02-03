ff43f73790bd05f74e05a2a7ba28b04036a69a76
Yannick Nzoda Situ <yannick.nzodasitu@cgi.com>
2022-02-03 08:06:56 -0500

    inial commit

--- src/calc.js
+++ src/calc.js
@@ -1,3 +1,4 @@
 const sum = (a,b) => a + b;
+const subtract = (a,b) => a - b;
 
-export {sum};
+export {subtract,sum};
--- src/calc.test.js
+++ src/calc.test.js
@@ -1,5 +1,32 @@
-import { sum } from "./calc";
+import {subtract, sum } from "./calc";
+
+beforeAll(()=> {
+    console.log("This will run  before all the test");
+})
+beforeEach(()=> {
+    console.log("This will be printed before each test");
+})
+
+afterEach(()=> {
+    console.log("This will be printed after each test");
+}) 
+
+afterAll(()=> {
+    console.log("This will run  after all the test");
+})   
 
 test('Adding 2 + 3 should give me 5', () => {
     expect(sum(2, 3)).toBe(5);
+})
+
+test('Subtract 2 from 3 that should give me a 1', () => {
+    expect(subtract(3, 2)).toBe(1);
+})
+
+describe('sum',()=>{
+    each([
+
+    ]).test('sum %s and %s should be %s', (a,b,result) => {
+        expect(sum(a,b)).toBe()
+    })
 })
